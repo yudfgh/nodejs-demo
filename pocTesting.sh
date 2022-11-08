64f9270f0278451d91762d0685668ce3e56c2b90
Prakhar Mishra <98946887+yudfgh@users.noreply.github.com>
2022-11-08 15:18:21 +0530

    Create pocTesting.sh

---
+++ pocTesting.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+while IFS="," read method api result extra;
+do
+if [[ "$extra" -eq 0 ]]
+then
+        echo Method:- $method, API:- $api, Result:- $result;
+        status=$(curl -so /dev/null -w '%{response_code}' $api) || true
+
+        echo Status :- $status
+
+        if [[ "$result" == "$status" ]]
+        then
+                echo "API Pass"
+
+
+        else
+
+                echo "API Fail"
+        fi
+else
+        echo Method:- $method, API:- $api, Result:- $result;
+        status=$(curl -so /dev/null -w '%{response_code}' $api) || true
+
+        status="unkown"
+
+        if [[ "$result" -eq "blackmask" ]]
+        then
+                        echo "API Pass"
+
+        else
+
+                        echo "API Fail"
+
+        fi
+
+fi
+done < data_csv.csv
